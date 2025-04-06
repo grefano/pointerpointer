2bef0f9985cf8604cdfa7dabeccf42f90a0fdb93
grefano <147395981+grefano@users.noreply.github.com>
2025-04-06 20:16:20 -0300

    fetch response more than one image
    client receives a list of images

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
-const img = document.getElementById('pointerimg')
+const img = document.getElementById('imgpointer')
+const txt = document.getElementById('txtdebug')
 // img.src = data.imageUrl
 
 /*
@@ -11,18 +12,51 @@ fetch('/api/img', {
     img.src = url
 });
 */
+var mousex = 0
+var mousey = 0 
+
+window.addEventListener('mousemove', function(e){
+    //console.log(`x: ${e.x} y: ${e.y}`)
+    mousex = e.x
+    mousey = e.y
+    
+    //console.log('uai')
+    
+})
 
 
 async function getImg(){
-    const response = await fetch('/api/img')
-    const data = await response.blob()
-    console.log(data)
 
+    console.log('getimg')
 
-    img.src=URL.createObjectURL(data)
+    fetch('/api/img', {
+        method: "POST",
+        //body: 10
+        //body: JSON.stringify({x:mousex, y:mousey})
+        body: JSON.stringify({texto:'bosta request'}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(imagens => {
+        const container = document.getElementsByClassName('imgbox')[0]
+        console.log('response')
+        imagens.forEach(image => {
+            console.log(image.url)
+            const el = document.createElement('img')
+            el.src = image.url
+            //img.src = image.url
+            container.appendChild(el)
+        });
+    })
+    .then(error => console.log(error))
+    
 
-    return data
+    //return data
 }
+
+
 
 
 getImg()
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express'
 const app = express()
 const serverUrl = 'C:/Users/USUARIO/documents/code/expressjs'
+const imagesUrl = serverUrl+'/public/imgs/'
+
+import fs from 'fs'
+import path from 'path'
+
 
 
 //app.use('/')
@@ -13,12 +18,64 @@ app.set('view engine', 'ejs')
 // html
 app.get('/', (req, res) => {
     console.log("here")
-    res.render('index', {img: "poggers"})
+    res.render('index')
 })
 
+function checkImage(file) {
+}
+
+app.use(express.json())
+
+
 // data
-app.get('/api/img', (req, res) => {
-    res.sendFile(serverUrl+'/public/imgs/75-79-26.png')
+app.post('/api/img', (req, res) => {
+    (async ()=>{
+        let qtd = 0
+        try {
+            const files = await fs.promises.readdir(imagesUrl)
+            const validFiles = []
+            //console.log(req.body)
+
+            for(const file of files){
+                //console.log('\n')
+                const filePath = path.join(imagesUrl, file) 
+                const stat = await fs.promises.stat(filePath)
+
+
+                const fileName = path.parse(path.basename(filePath)).name
+                
+                
+                const fileValuesList = fileName.split("-")
+                const fileValues = {
+                    x:      Number(fileValuesList[0]),
+                    y:      Number(fileValuesList[1]),
+                    dir:    Number(fileValuesList[2])
+                }
+                
+                if (qtd < 5){
+                    console.log(`qtd images sending ${qtd}`)
+                    validFiles.push({
+                        url: `/imgs/${file}`,
+                        x: fileValues.x,
+                        y: fileValues.y,
+                        dir: fileValues.dir,
+                    })
+                    qtd++    
+                }
+
+               
+            }
+
+            res.send(validFiles)
+        } 
+        catch (e){
+            console.log('error image bosta')
+            console.log(e)
+            console.log(req.body)
+        }
+    })();
+
+    //res.sendFile(serverUrl+'/public/imgs/75-79-26.png')
 })
 
 
